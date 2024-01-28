5170327160702bd726aaf309bd1b96934599d9ca
kzcheng <kevinzifancheng@gmail.com>
2024-01-28 09:08:54 -0800

    Initializing project with Java Springboot

---
+++ src/test/java/cmpt276/kzcheng/asn1/Asn1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cmpt276.kzcheng.asn1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Asn1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
