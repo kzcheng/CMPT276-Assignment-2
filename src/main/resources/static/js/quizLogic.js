324d9c5a83fc3f9f90175bfc1d9e775fb84f0f6e
kzcheng <kevinzifancheng@gmail.com>
2024-02-02 08:10:14 -0800

    Changed tag name and removed obsolete code

--- src/main/resources/static/js/quizLogic.js
+++ src/main/resources/static/js/quizLogic.js
@@ -1,50 +1,3 @@
-// This is a global variable that will be used to store the questions and answers.
-// Also this is a bad idea so I commented it out.
-// var questions = [
-//     {
-//         // Ok, I'll admit that the process of loading the questions and answers is... pretty stupid and obviously AI generated.
-//         // But... it works and it'll do for now.
-//         question: document.querySelector("#question1 .question").textContent,
-//         answers: [...document.querySelectorAll('#question1 .choices button')].reduce((obj, button, index) => {
-//             obj[index + 1] = button.textContent
-//             return obj
-//           }, {}),
-//         correctAnswer: 1,
-//     },
-//     {
-//         question: document.querySelector("#question2 .question").textContent,
-//         answers: [...document.querySelectorAll('#question2 .choices button')].reduce((obj, button, index) => {
-//             obj[index + 1] = button.textContent
-//             return obj
-//           }, {}),
-//         correctAnswer: 2,
-//     },
-//     {
-//         question: document.querySelector("#question3 .question").textContent,
-//         answers: [...document.querySelectorAll('#question3 .choices button')].reduce((obj, button, index) => {
-//             obj[index + 1] = button.textContent
-//             return obj
-//           }, {}),
-//         correctAnswer: 3,
-//     },
-//     {
-//         question: document.querySelector("#question4 .question").textContent,
-//         answers: [...document.querySelectorAll('#question4 .choices button')].reduce((obj, button, index) => {
-//             obj[index + 1] = button.textContent
-//             return obj
-//           }, {}),
-//         correctAnswer: 4,
-//     },
-//     {
-//         question: document.querySelector("#question5 .question").textContent,
-//         answers: [...document.querySelectorAll('#question5 .choices button')].reduce((obj, button, index) => {
-//             obj[index + 1] = button.textContent
-//             return obj
-//           }, {}),
-//         correctAnswer: 5,
-//     },
-// ];
-
 //
 //
 // Variables
@@ -76,7 +29,7 @@ function loadQuestionDataFromHTML(qList) {
         // I guess it should be fine? Since qList itself is declared with var.
         qList[i] = {}
         qList[i].self = document.querySelector(`#q${i}`)
-        qList[i].qText = qList[i].self.querySelector(".question").textContent
+        qList[i].qText = qList[i].self.querySelector(".qText").textContent
         qList[i].aList = [...qList[i].self.querySelectorAll(".choices button")].reduce((obj, button, index) => {
             obj[index + 1] = button.textContent
             return obj
