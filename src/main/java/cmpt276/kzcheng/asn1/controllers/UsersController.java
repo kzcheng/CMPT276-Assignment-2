55fdd90d62692ecd8b1911806440589be36fa41b
kzcheng <kevinzifancheng@gmail.com>
2024-02-11 23:10:54 -0800

    Completed Tutorial 7 Postgres Database on Render

--- src/main/java/cmpt276/kzcheng/asn1/controllers/UsersController.java
+++ src/main/java/cmpt276/kzcheng/asn1/controllers/UsersController.java
@@ -2,26 +2,45 @@ package cmpt276.kzcheng.asn1.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map; // Correct Map import
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.observation.ObservationProperties.Http;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import cmpt276.kzcheng.asn1.models.Users;
+import cmpt276.kzcheng.asn1.models.User;
+import cmpt276.kzcheng.asn1.models.UserRepository;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 public class UsersController {
+    @Autowired
+    private UserRepository userRepo;
+
     @GetMapping("/users/view")
     public String getAllUsers(Model model) {
         System.out.println("Get all users");
-        // TODO: Get all users from the database
-        List<Users> users = new ArrayList<>();
-        users.add(new Users("Bobby", "1234", 40));
-        users.add(new Users("Steve", "4321", 30));
-        users.add(new Users("Jack", "4322", 40));
-        users.add(new Users("Bob", "1231", 50));
+        // Get all users from the database
+        List<User> users = userRepo.findAll();
         // End of database call.
         model.addAttribute("us", users);
         return "users/showAll";
         // Links to the file in resources/templates/users/showAll.html
+    }
+
+    @PostMapping("/users/add")
+    public String addUser(@RequestParam Map<String, String> newuser, HttpServletResponse response) {
+        System.out.println("ADD user");
+        String newName = newuser.get("name");
+        String newPassword = newuser.get("password");
+        int newSize = Integer.parseInt(newuser.get("size"));
+        userRepo.save(new User(newName, newPassword, newSize));
+        response.setStatus(HttpServletResponse.SC_CREATED);
+        return "users/addedUser";
     }
 }
