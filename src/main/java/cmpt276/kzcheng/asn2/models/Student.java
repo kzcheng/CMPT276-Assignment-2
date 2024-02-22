b71f4f0b14f501afea5a67afff47721d13d09be3
kzcheng <kevinzifancheng@gmail.com>
2024-02-22 14:52:52 -0800

    Created Student in models

---
+++ src/main/java/cmpt276/kzcheng/asn2/models/Student.java
@@ -0,0 +1,77 @@
+package cmpt276.kzcheng.asn2.models;
+
+import jakarta.persistence.*;
+
+/**
+ * Represents a student with various attributes such as name, weight, height, hair color, and GPA.
+ */
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int sid; // student id
+    private String name;
+    private int weight;
+    private int height;
+    private String hairColor;
+    private float gpa;
+
+    public Student() {}
+
+    public Student(String name, int weight, int height, String hairColor, float gpa) {
+        this.name = name;
+        this.weight = weight;
+        this.height = height;
+        this.hairColor = hairColor;
+        this.gpa = gpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public String getHairColor() {
+        return hairColor;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public float getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(float gpa) {
+        this.gpa = gpa;
+    }
+
+    public int getSid() {
+        return sid;
+    }
+
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+
+
+}
