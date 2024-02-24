c7c0fabe2f21b12f86cbd0a1e93cc57340bfcf09
kzcheng <kevinzifancheng@gmail.com>
2024-02-24 15:36:44 -0800

    Implemented global variable currentSelectedStudentID in JS

--- src/main/resources/static/js/student.js
+++ src/main/resources/static/js/student.js
@@ -1,6 +1,15 @@
 console.log("student.js loaded.")
 
+// -- Global Variables and Main Code --
+var currentSelectedStudentID = -1
+
+// -- Functions --
+function selectedStudent(button) {
+    currentSelectedStudentID = button.getAttribute("sid")
+    console.log("Current Selected Student ID: ", currentSelectedStudentID)
+}
+
 function randomShitGotClicked(button) {
-    var sid = button.getAttribute('sid');
-    console.log("Shit", sid);
+    var sid = button.getAttribute("sid")
+    console.log("Shit", sid)
 }
