687d08e82d384633028c03bc0034d1670b2b265c
kzcheng <kevinzifancheng@gmail.com>
2024-02-24 12:36:19 -0800

    Refactor: Changed how import works

--- src/main/java/cmpt276/kzcheng/asn2/controllers/StudentController.java
+++ src/main/java/cmpt276/kzcheng/asn2/controllers/StudentController.java
@@ -2,20 +2,14 @@ package cmpt276.kzcheng.asn2.controllers;
 
 import java.util.List;
 import java.util.Map;
+import jakarta.servlet.http.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
 import cmpt276.kzcheng.asn2.models.Student;
 import cmpt276.kzcheng.asn2.models.StudentRepository;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class StudentController {
